3eb2b30515c9bf3503e0d7283c792c26d6bfbd86
Tjitse Efdé <tjitse@vendic.nl>
2019-06-08 00:35:27 +0200

    Refactoring test

--- test/api/extensions/mail-service.spec.ts
+++ test/api/extensions/mail-service.spec.ts
@@ -1,24 +1,41 @@
 import {expect} from 'chai'
-import 'chai'
-import 'chai-http'
-import 'mocha'
-
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import config from 'config'
 
 chai.use(chaiHttp);
 
-
 describe('Mail service token request', () => {
 
-  it('should return md5 token and status 200', async () => {
-
-    let response = await chai.request('http://localhost:8080')
+  it('should return token and status 200', async () => {
+    const host = config.get('server.host')
+    const port = config.get('server.port')
+    const response = await chai.request(`${host}:${port}`)
       .get('/api/ext/mail-service/get-token')
+    const token = response.body.result
 
     expect(response.body.code).to.equal(200)
-    expect(typeof response.body.result === "string").to.equal(true)
-
+    expect(typeof token === "string").to.equal(true)
   })
 
+  /*
+  it('should return token and status 200', async () => {
+
+    const host = config.get('server.host')
+    const port = config.get('server.port')
+    const tokenResponse = await chai.request(`${host}:${port}`)
+      .get('/api/ext/mail-service/get-token')
+    const token = tokenResponse.body.result
+
+    const mailResponse = await chai.request(`${host}:${port}`)
+      .post('/api/ext/mail-service/send-email')
+      .send({
+        'token': token
+      })
+
+    expect(mailResponse.body.code).to.equal(200)
+  })
+  */
 })
+
+
