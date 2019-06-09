5ab1a5f12e23d4f1cc2ca30fc867c805090f96f5
Tjitse Efdé <tjitse@vendic.nl>
2019-06-09 10:15:13 +0200

    Refactored mail service test

--- src/api/extensions/mail-service/test/mail-service.spec.ts
+++ src/api/extensions/mail-service/test/mail-service.spec.ts
@@ -5,24 +5,26 @@ import config from 'config'
 
 chai.use(chaiHttp);
 
-describe('Mail service token request', () => {
+describe('Testing mail service token endpoint', () => {
 
   it('should return token and status 200', async () => {
     const host = config.get('server.host')
     const port = config.get('server.port')
-    const response = await chai.request(`${host}:${port}`)
-      .get('/api/ext/mail-service/get-token')
+    const response = await tokenRequest(host, port)
     const token = response.body.result
 
     expect(response.body.code).to.equal(200)
     expect(typeof token === "string").to.equal(true)
   })
+})
+
+describe('Testing mail service send-email endpont', () => {
 
   it('should return not in whitelist and status 500', async () => {
+
     const host = config.get('server.host')
     const port = config.get('server.port')
-    const tokenResponse = await chai.request(`${host}:${port}`)
-      .get('/api/ext/mail-service/get-token')
+    const tokenResponse = await tokenRequest(host, port);
     const token = tokenResponse.body.result
 
     const mailResponse = await chai.request(`${host}:${port}`)
@@ -34,10 +36,22 @@ describe('Mail service token request', () => {
 
       })
 
-    expect(mailResponse.body.code).to.equal(500)
-    expect(mailResponse.body.result).to.equal('Target email address (not-in-whitelist@vuestorefront.io) is not from the whitelist!')
+    expect(mailResponse.body.code).to.equal(500, 'Expecting code 500 response')
+    expect(mailResponse.body.result).to.equal(
+      'Target email address (not-in-whitelist@vuestorefront.io) is not from the whitelist!',
+      'Expecting target email address error'
+    )
   })
 
 })
+
+/**
+ * @param host : string
+ * @param port : number
+ */
+async function tokenRequest(host, port) {
+  return await chai.request(`${host}:${port}`)
+    .get('/api/ext/mail-service/get-token')
+}
 
 
