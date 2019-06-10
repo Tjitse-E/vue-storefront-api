f475a7d7ef99ca9e9a98286765806da42d6284fb
Tjitse Efdé <tjitse@vendic.nl>
2019-06-10 15:24:50 +0200

    Testing jest and jest --inspect

---
+++ foo.test.ts
@@ -0,0 +1,15 @@
+describe('Testing mail service token endpoint', () => {
+
+    test('basic', () => {
+        const numberOne = 10
+        const numberTwo = 20
+        const numberThree = 30
+
+        expect(numberOne + numberTwo).toBe(numberThree)
+    });
+
+    test('basic again', () => {
+        expect(3).toBe(3)
+    });
+
+})
